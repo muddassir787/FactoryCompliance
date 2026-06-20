77fb5b57a1514e75b2c4655eec117a81626b01ca
muddassir787 <mudasir.25022004@gmail.com>
2026-06-20 17:08:26 +0500

    Add files via upload

---
+++ src/policy/rules.py
@@ -0,0 +1,39 @@
+RULES = {
+
+"walkway_violation":{
+
+"section":"3.3.2",
+
+"description":
+"person outside green walkway"
+
+},
+
+"unauthorized_intervention":{
+
+"section":"4.3.2",
+
+"description":
+"person touching machine without green vest"
+
+},
+
+"opened_panel":{
+
+"section":"5.2.2",
+
+"description":
+"electrical panel open"
+
+},
+
+"forklift_overload":{
+
+"section":"6.3.2",
+
+"description":
+"forklift carrying >= 3 blocks"
+
+}
+
+}
